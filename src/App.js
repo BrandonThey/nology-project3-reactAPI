44edbe8e3ed54ddd22898ef9ed886497983d3e3a
Brandon They <theyb5220@gmail.com>
2022-08-16 15:59:41 -0500

    Fetched beer data from the api for use in the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-
 import './App.css';
 import SideNavBar from './containers/SideNavBar/SideNavBar';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [beers, setBeers] = useState();
+
+  const getBeers = () => {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setBeers(data)
+      })
+  }
+
+  useEffect(getBeers, []);
+
+  useEffect(() => {
+    console.log(beers)
+  },[beers])
+
   return (
     <div className="App">
       <SideNavBar />
---
+++ src/componets/BeerCard/BeerCard.jsx
@@ -0,0 +1,16 @@
+import "./BeerCard.scss"
+const BeerCard = (props) => {
+    const {beersArr} = props;
+
+    // const mappedBeers = beersArr.map((beer) => {
+
+    // })
+
+    return(
+        <>
+            {/* {mappedBeers} */}
+        </>
+    )
+}
+
+export default BeerCard;
