cb07a19f2425a4addaf100629ff0050b3e0ba943
Brandon They <theyb5220@gmail.com>
2022-08-17 12:14:17 -0500

    Added SearchBox functionality to app.jsx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { useState, useEffect } from 'react';
 function App() {
 
   const [beers, setBeers] = useState();
-
+  const [searchTerm, setSearchTerm] = useState("");
+  let filteredBeers;
   const getBeers = () => {
     fetch("https://api.punkapi.com/v2/beers")
       .then((response) => {
@@ -20,16 +21,29 @@ function App() {
 
   useEffect(getBeers, []);
 
+  const handleInput = (event) => {
+    const cleanInput = event.target.value.toLowerCase();
+    setSearchTerm(cleanInput);
+  };
+
+  if(beers !== undefined){
+    filteredBeers = beers.filter((beer) => {
+      const beerLower = beer.name.toLowerCase();
+  
+      return beerLower.includes(searchTerm);
+    });
+  }
+
   return (
     <div className="App">
       <header>Header</header>
       <main>
         <section className='side-nav'>
-          <SideNavBar />
+          {beers && <SideNavBar searchTerm={searchTerm} handleInput={handleInput}/>}
         </section>
 
         <section className='beer-cards'>
-          {beers && <BeerCard beersArr={beers}/>}
+          {beers && <BeerCard beersArr={filteredBeers}/>}
         </section>
       </main>
     </div>
--- src/containers/SideNavBar/SideNavBar.jsx
+++ src/containers/SideNavBar/SideNavBar.jsx
@@ -1,11 +1,14 @@
 import Button from "../../componets/Button/Button";
 import SearchBox from "../../componets/SearchBox/SearchBox";
+import BeerCard from "../../componets/BeerCard/BeerCard";
 import "./SideNavBar.scss"
-const SideNavBar = () => {
+
+const SideNavBar = (props) => {
+    const {handleInput, searchTerm} = props;
 
     return(
         <>
-            <SearchBox />
+            <SearchBox label={"Search By Name!"} searchTerm={searchTerm} handleInput={handleInput}/>
             <Button title="All Beers" link=""/>
             <Button title="High ABV" link="highABV"/>
             <Button title="Classic Range" link="classicRange"/>
